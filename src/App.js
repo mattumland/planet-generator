438ff3e77db72a93d443be13b75299abb4ab0739
Matt Umland <mattumland@gmail.com>
2022-07-23 16:21:26 -0400

    Add json file, display data in specific location

--- src/App.js
+++ src/App.js
@@ -1,11 +1,52 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import rawPlanetData from './data/csvjson.json'
+import _ from "lodash";
+import { wardenFilters } from './helpers/infoOrganizer';
+import { wardenHeadings, playerHeadings } from './data/headingLists';
 
 function App() {
+  const [planetData, setPlanetData] = useState(rawPlanetData)
+
+  const wardenInfo = wardenHeadings.map(heading => {
+    return (
+      <div
+        key={heading}
+      >
+        <h3>{heading}</h3>
+        <p>{planetData[0][heading]}</p>
+      </div>
+    )
+  })
+
+    const playerInfo = playerHeadings.map(heading => {
+    return (
+      <div
+        key={heading}
+      >
+        <h3>{heading}</h3>
+        <p>{planetData[0][heading]}</p>
+      </div>
+    )
+  })
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Planet Generator</h1>
+        <div>
+          <h2>Details</h2>
+        </div>
+        <div>
+          <h2>Warden Info</h2>
+            {wardenInfo}
+        </div>
+        <div>
+          <h2>Player Info</h2>
+          {playerInfo}
+
+        </div>
+
       </header>
     </div>
   );
---
+++ src/data/headingLists.js
@@ -0,0 +1,3 @@
+export const wardenHeadings = ["flora & fauna", "mysteries & phenomena", "hooks & seeds"]
+
+export const playerHeadings = ["water prevelance (% of surface covered)", "average temperature (f)", "terrain", "atmosphere", "gravity (% of earth)"]
